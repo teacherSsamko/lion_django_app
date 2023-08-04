ce7aac554d1dc54dea2d9d4883f31e31cc5806a4
Eunsub Lee <ssamko@kakao.com>
2023-08-04 09:52:53 +0900

    INIT Forum

---
+++ lion_app/Forum/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.TextField(max_length=128, unique=True)
+    is_private = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.TextField(max_length=200)
+    content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
