7d982df8ab3e86bd589cf2243b74365509a3fb3f
Eunsub Lee <ssamko@kakao.com>
2023-08-07 15:05:21 +0900

    settings.py: FIX DATABASES.default

--- lion_app/lion_app/settings.py
+++ lion_app/lion_app/settings.py
@@ -93,8 +93,11 @@ WSGI_APPLICATION = 'lion_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
     }
 }
 
