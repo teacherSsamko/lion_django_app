2886035b8402a52620f80974038f554b5c8f928d
Eunsub Lee <ssamko@kakao.com>
2023-08-10 15:33:34 +0900

    Forum.views: ADD extend_schema

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,9 +1,11 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
+@extend_schema(tags=["Topic"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
