c59cba2766cb2d72e7ad7ab8b0113e4bc8a46dbc
Eunsub Lee <ssamko@kakao.com>
2023-08-11 11:20:35 +0900

    Forum.views: FIX schema

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -5,13 +5,17 @@ from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
-@extend_schema(tags=["Topic", "Forum"])
+@extend_schema(tags=["Topic"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
 
+    @extend_schema(summary="새 토픽 생성")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
-@extend_schema(tags=["Post", "Forum"])
+
+@extend_schema(tags=["Post"])
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
