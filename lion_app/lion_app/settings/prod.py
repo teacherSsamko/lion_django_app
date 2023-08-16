af265b9f2c730024b09dac64c68fbef3ec68a368
Eunsub Lee <ssamko@kakao.com>
2023-08-16 11:09:57 +0900

    REFACTOR: settings

---
+++ lion_app/lion_app/settings/prod.py
@@ -0,0 +1,14 @@
+from base import *
+
+
+SECRET_KEY = ""
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "lion-lb-18904307-df5b6f497fc4.kr.lb.naverncp.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://lion-lb-18904307-df5b6f497fc4.kr.lb.naverncp.com",
+]
---
+++ lion_app/lion_app/settings/staging.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "lion-lb-staging-18975820-e3aa639c372a.kr.lb.naverncp.com",
+]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://lion-lb-staging-18975820-e3aa639c372a.kr.lb.naverncp.com",
+# ]
