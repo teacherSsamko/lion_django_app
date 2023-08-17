76243f37ecdde5314755b14c43f3d232fa8d8071
Eunsub Lee <ssamko@kakao.com>
2023-08-17 10:11:10 +0900

    Forum.views: FEAT Post.create

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-from .models import Topic, Post
+from .models import Topic, Post, TopicGroupUser
 from .serializers import TopicSerializer, PostSerializer
 
 
@@ -19,3 +23,22 @@ class TopicViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        topic_id = data.get("topic")
+        topic = get_object_or_404(Topic, id=topic_id)
+        if topic.is_private:
+            qs = TopicGroupUser.objects.filter(
+                group__lte=TopicGroupUser.groupChoices.common,
+                topic=topic,
+                user=user,
+            )
+            if not qs.exists():
+                return Response(
+                    status=status.HTTP_401_UNAUTHORIZED,
+                    data="This user is not allowed to write a post on this topic",
+                )
+
+        return super().create(request, *args, **kwargs)
