def8cde12b18fa8b2ddc87a875f99f9b4fba6831
Eunsub Lee <ssamko@kakao.com>
2023-08-17 11:16:30 +0900

    settings.base: FIX DRF.DEFAULT_PERMISSION_CLASSES

--- lion_app/lion_app/settings/base.py
+++ lion_app/lion_app/settings/base.py
@@ -157,9 +157,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
