14e916a6441da8df87a71612a0f9b378775a7225
Eunsub Lee <ssamko@kakao.com>
2023-08-18 11:08:38 +0900

    Froum.views: FIX post-list

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -40,6 +40,10 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    @extend_schema(deprecated=True)
+    def list(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_400_BAD_REQUEST, data="Deprecated API")
+
     def create(self, request: Request, *args, **kwargs):
         user = request.user
         data = request.data
