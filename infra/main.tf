a6a5b02c7c4622476334158b99c2bc5e6f6701fa
Eunsub Lee <ssamko@kakao.com>
2023-08-21 11:23:21 +0900

    infra.main: Init

---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    ncloud = {
+      source = "NaverCloudPlatform/ncloud"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "ncloud" {
+  region      = "KR"
+  site = "PUBLIC"
+  support_vpc = true
+}
+
+resource "ncloud_login_key" "loginkey" {
+  key_name = "test-key"
+}
+
+resource "ncloud_vpc" "test" {
+  ipv4_cidr_block = "10.1.0.0/16"
+}
+
+resource "ncloud_subnet" "test" {
+  vpc_no         = ncloud_vpc.test.vpc_no
+  subnet         = cidrsubnet(ncloud_vpc.test.ipv4_cidr_block, 8, 1)
+  zone           = "KR-2"
+  network_acl_no = ncloud_vpc.test.default_network_acl_no
+  subnet_type    = "PUBLIC"
+  usage_type     = "GEN"
+}
+
+resource "ncloud_server" "server" {
+  subnet_no                 = ncloud_subnet.test.id
+  name                      = "my-tf-server"
+  server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
+  login_key_name            = ncloud_login_key.loginkey.key_name
+}
