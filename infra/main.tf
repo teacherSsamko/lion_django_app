47d0906a31ab78631dc61f64048baa60df2accf1
Eunsub Lee <ssamko@kakao.com>
2023-08-22 15:12:42 +0900

    infra.main: ADD loadBalancer

--- infra/main.tf
+++ infra/main.tf
@@ -203,3 +203,56 @@ resource "ncloud_public_ip" "db" {
 output "db_public_ip" {
   value = ncloud_public_ip.db.public_ip
 }
+
+## LoadBalancer
+
+# subnet
+resource "ncloud_subnet" "be-lb" {
+  vpc_no         = ncloud_vpc.main.id
+  subnet         = cidrsubnet(ncloud_vpc.main.ipv4_cidr_block, 8, 2)
+  zone           = "KR-2"
+  network_acl_no = ncloud_vpc.main.default_network_acl_no
+  subnet_type    = "PRIVATE"
+  name           = "be-lb-subnet"
+  usage_type     = "LOADB"
+}
+
+# LoadBalancer
+resource "ncloud_lb" "be-staging" {
+  name = "be-lb-staging"
+  network_type = "PUBLIC"
+  type = "NETWORK_PROXY"
+  subnet_no_list = [ ncloud_subnet.be-lb.subnet_no ]
+}
+
+resource "ncloud_lb_listener" "be" {
+  load_balancer_no = ncloud_lb.be-staging.load_balancer_no
+  protocol = "TCP"
+  port = 80
+  target_group_no = ncloud_lb_target_group.be.target_group_no
+}
+
+# target group
+resource "ncloud_lb_target_group" "be" {
+  vpc_no   = ncloud_vpc.main.vpc_no
+  protocol = "PROXY_TCP"
+  target_type = "VSVR"
+  port        = 8000
+  description = "for django be"
+  health_check {
+    protocol = "TCP"
+    http_method = "GET"
+    port           = 8000
+    url_path       = "/admin"
+    cycle          = 30
+    up_threshold   = 2
+    down_threshold = 2
+  }
+  algorithm_type = "RR"
+}
+
+# target group attachment
+resource "ncloud_lb_target_group_attachment" "be" {
+  target_group_no = ncloud_lb_target_group.be.target_group_no
+  target_no_list = [ncloud_server.be.instance_no]
+}
