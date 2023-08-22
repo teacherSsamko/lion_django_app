6902325cd153123c8fb235bcd22d3f6768fff04d
Eunsub Lee <ssamko@kakao.com>
2023-08-22 15:28:25 +0900

    infra.main: var password

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,10 @@ provider "ncloud" {
   support_vpc = true
 }
 
+variable "password" {
+  type = string
+}
+
 resource "ncloud_login_key" "loginkey" {
   key_name = "lion-test-key"
 }
@@ -77,35 +81,9 @@ resource "ncloud_server" "be" {
 
 resource "ncloud_init_script" "main" {
   name    = "set-server-tf"
-  content = <<EOT
-#!/bin/bash
-
-USERNAME="lion"
-PASSWORD="1212"
-REMOTE_DIRECTORY="/home/$USERNAME/"
-
-echo "Add user"
-useradd -s /bin/bash -d $REMOTE_DIRECTORY -m $USERNAME
-
-echo "Set password"
-echo "$USERNAME:$PASSWORD" | chpasswd
-
-echo "Set sudo"
-usermod -aG sudo $USERNAME
-echo "$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME
-
-echo "Update apt and Install docker & docker-compose"
-sudo apt-get update
-sudo apt install -y docker.io docker-compose
-
-echo "Start docker"
-sudo service docker start && sudo service docker enable
-
-echo "Add user to 'docker' group"
-sudo usermod -aG docker $USERNAME
-
-echo "done"
-EOT
+  content = templatefile("${path.module}/main_init_script.tftpl", {
+    password = var.password
+  })
 }
 
 resource "ncloud_public_ip" "be" {
