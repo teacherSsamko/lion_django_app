af8bbc52c191b65ecd03478aa35e000508be3723
Eunsub Lee <ssamko@kakao.com>
2023-08-31 11:46:37 +0900

    Forum.views: ADD destroy permission check

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -114,3 +114,22 @@ class PostViewSet(viewsets.ModelViewSet):
             )
 
         return super().retrieve(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        post: Post = self.get_object()
+        topic: Topic = post.topic
+        if (
+            topic.owner == request.user
+            or post.owner == request.user
+            or TopicGroupUser.objects.filter(
+                user=request.user,
+                group=TopicGroupUser.GroupChoices.admin,
+                topic=topic,
+            ).exists()
+        ):
+            return super().destroy(request, *args, **kwargs)
+        else:
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data="This user is not allowed to delete this post",
+            )
