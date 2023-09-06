80393610534ec9cea449eb727b4bf9dedef81440
Eunsub Lee <ssamko@kakao.com>
2023-09-06 19:33:31 +0900

    settings.staging: allow all hosts

--- lion_app/lion_app/settings/staging.py
+++ lion_app/lion_app/settings/staging.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "lion-lb-staging-18975820-e3aa639c372a.kr.lb.naverncp.com",
+    "*",
 ]
 
 # CSRF_TRUSTED_ORIGINS = [
