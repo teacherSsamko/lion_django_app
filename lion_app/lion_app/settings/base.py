f1791c67f3b5a692f643421c1c7f967c680d6bad
Eunsub Lee <ssamko@kakao.com>
2023-09-19 11:26:37 +0900

    base: fix version

--- lion_app/lion_app/settings/base.py
+++ lion_app/lion_app/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
