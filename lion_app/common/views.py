3d04ae93905b2f1ad91d15b73061719c78ecdb4a
Eunsub Lee <ssamko@kakao.com>
2023-09-21 13:06:17 +0900

    common.views: FEAT Me

--- lion_app/common/views.py
+++ lion_app/common/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse, HttpResponse
 from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 
 def healthcheck(request):
@@ -8,3 +11,8 @@ def healthcheck(request):
 
 def get_version(request):
     return JsonResponse({"version": settings.VERSION})
+
+
+class Me(APIView):
+    def get(self, request: Request):
+        return Response({"pk": request.user.pk})
